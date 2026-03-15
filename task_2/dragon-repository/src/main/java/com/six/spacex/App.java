faad3a7879d3ae6991c9c43f9ee918bd73760c23
mathieu <delphi.mat@gmail.com>
2026-03-15 13:43:12 +0100

    build: init maven project and configure TDD dependencies
    - Set up the initial pom.xml for a pure Java lib, packaging as a jar.
    - Add JUnit 5 and AssertJ test dependencies to support Test-Driven Development.
    - Configure maven-compiler-plugin and maven-surefire-plugin for Java 11.
    - No web or enterprise frameworks are included

---
+++ task_2/dragon-repository/src/main/java/com/six/spacex/App.java
@@ -0,0 +1,13 @@
+package com.six.spacex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
